55ab3fb2cb68b187276749917b8c5cb24bebe0ed
itggot-saga-losman <38528635+itggot-saga-losman@users.noreply.github.com>
2018-04-26 14:41:56 +0200

    Made "max_of_four.rb"

--- lib/max_of_four.rb
+++ lib/max_of_four.rb
@@ -3,17 +3,18 @@
 # input1 - Number 1.
 # input2 - Number 2.
 # input3 - Number 3.
+# input4 - Number 4.
 #
 # Examples
 #
-#   between_strict(2, 3, 5)
+#   between_strict(2, 3, 5, 4)
 #   # => 5
 #
-#   between_strict(6, 2, 10)
+#   between_strict(6, 2, 10, 9)
 #   # => 10
 #
 # Returns biggest input.
-def max_of_three(input1, input2, input3)
+def max_of_four(input1, input2, input3, input4)
     biggest = input2
     if input1 > input2
         biggest = input1
@@ -21,5 +22,10 @@ def max_of_three(input1, input2, input3)
     if input3 > biggest
         biggest = input3
     end
+    if input4 > biggest
+        biggest = input4
+    end
     return biggest
 end
+
+puts max_of_four(2,5,89,3)
